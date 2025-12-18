ac9f853cac2607714a7dca6dcdaf5d06d7a68850
Kaur <gagandeepmalh@gmail.com>
2025-12-18 00:21:52 +0000

    docs: Add detailed comments to Tailwind config explaining dark mode setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+// Tailwind CSS configuration file
 export default {
+  // Files to scan for Tailwind classes
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}', // All JS/JSX/TS/TSX files in src folder
   ],
+
+  // NEW: Enable dark mode using 'class' strategy
+  // This means dark mode is activated when 'dark' class is added to <html> element
+  darkMode: 'class',
+
   theme: {
-    extend: {},
+    extend: {}, // Custom theme extensions (none currently)
   },
-  plugins: [],
+  plugins: [], // Tailwind plugins (none currently)
 };
